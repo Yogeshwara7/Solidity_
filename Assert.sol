138483fadf923392fce8c85b1f637cb6c630cd08
Yogeshwara B <yogeshwara567@gmail.com>
2026-01-10 21:46:43 +0530

    assert exercise

---
+++ Assert.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract assertex{
+// assert are conditional checker if the condtion is true then only the function wil executed or
+// else the function execution will fail
+    function exercise() public pure  {
+
+        assert(1 minutes==60 seconds);
+
+        assert(1 weeks == 7 days);
+
+        assert(1 days == 24 hours);
+
+    }
+
+    
+}
