51e2da64b2d7f47f20947e1ad17ea1e5c7d4aaf1
Yogeshwara B <yogeshwara567@gmail.com>
2026-01-24 18:14:30 +0530

    adding condition for ownerChange

---
+++ Owner2.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract Assigment{
+    mapping(address=>uint) balance;
+
+    address public owner;
+    constructor(){
+    owner=msg.sender;
+    }
+    modifier costs(uint _amount){
+        require(msg.value>=_amount, "Not Enough Ether");
+        _;
+    }
+
+    function forceOwnerChange(address _newOwner)external payable costs(99 ether){
+        owner = _newOwner;
+    }
+    function sendeth(address _owner, uint amount) external payable {
+        payable(_owner).transfer(amount);
+    }
+    function deposit() external payable {
+        balance[msg.sender] += msg.value;
+    }
+}
