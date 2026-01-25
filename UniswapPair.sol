d82ec714773c2ad3575358ed9f8efc25e06ad5a5
Yogeshwara B <yogeshwara567@gmail.com>
2026-01-25 18:58:49 +0530

    Uniswap Pair and get reserve assignment

---
+++ UniswapPair.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+interface Uniswap2Factory {
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    
+}
+
+interface UniswapV2Pair {
+
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    
+}
+
+contract Pair{
+
+    address constant FactoryAddress = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    function getPairAddress() public view returns(address){
+        address pair= Uniswap2Factory(FactoryAddress).getPair(USDC, WETH);
+
+        return pair;
+    }
+
+    function getReserves() view public returns(uint112 reserve0, uint112 reserve1){
+
+       address pair=getPairAddress();
+       require(pair!=address(0),"Pair does not exist");
+
+       (reserve0, reserve1,)= UniswapV2Pair(pair).getReserves();
+    }
+
+
+    
+    
+    }
