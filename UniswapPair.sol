75566ff0d70a17fa0c19c4c5a626ef39c6b7760d
Yogeshwara B <yogeshwara567@gmail.com>
2026-01-26 21:48:00 +0530

    updated code

--- UniswapPair.sol
+++ UniswapPair.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
+// Interface for Uniswap V2 Factory
 interface Uniswap2Factory {
 
     function getPair(address tokenA, address tokenB) external view returns (address pair);
     
 }
-
+// Interface for Uniswap V2 Pair
 interface UniswapV2Pair {
 
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
     
 }
-
+// Main contract to interact with Uniswap pairs
 contract Pair{
 
     address constant FactoryAddress = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
